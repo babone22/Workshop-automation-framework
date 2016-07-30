4dfd9b77a0611165483eeb42c511bb2081fc67f6
babone22 <il.alexandru@yahoo.com>
2016-07-30 19:24:04 -0300

    testing github connection

---
+++ src/test/java/pageobjects/ANewClassForTest.java
@@ -0,0 +1,11 @@
+package pageobjects;
+
+/**
+ * Created by MacBook on 30/07/16.
+ */
+public class ANewClassForTest {
+
+    public void JustForTest(){
+        System.out.print("Testing");
+    }
+}
